2b01c046e937bb4ad423651b42c8ff06832834fa
Ignacio Piantanida <ijpiantanida@gmail.com>
2017-03-02 01:01:56 -0300

    Use emit event instead of this-compilation
    Given that compression is most likely one of the last steps on the pipeline
    chain, it makes sense that this plugin uses the `emit` event which is the last
    step to add/modify assets before they're written to disk.
    
    This plays nicely with other plugins that create and delete files
    on the fly AFTER they're compiled
    (for example https://github.com/jtefera/merge-files-webpack, which is the
    recommeded solution for this issue
    https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/179)

--- index.js
+++ index.js
@@ -52,37 +52,36 @@ function CompressionPlugin(options) {
 module.exports = CompressionPlugin;
 
 CompressionPlugin.prototype.apply = function(compiler) {
-	compiler.plugin("this-compilation", function(compilation) {
-		compilation.plugin("optimize-assets", function(assets, callback) {
-			async.forEach(Object.keys(assets), function(file, callback) {
-				if(Array.isArray(this.test)) {
-					if(this.test.every(function(t) {
-						return !t.test(file);
-					})) return callback();
-				} else if(this.test && !this.test.test(file))
-					return callback();
-				var asset = assets[file];
-				var content = asset.source();
-				if(!Buffer.isBuffer(content))
-					content = new Buffer(content, "utf-8");
-				var originalSize = content.length;
-				if(originalSize < this.threshold) return callback();
-				this.algorithm(content, this.compressionOptions, function(err, result) {
-					if(err) return callback(err);
-					if(result.length / originalSize > this.minRatio) return callback();
-					var parse = url.parse(file);
-					var sub = {
-						file: file,
-						path: parse.pathname,
-						query: parse.query || ""
-					};
-					var newFile = this.asset.replace(/\[(file|path|query)\]/g, function(p0,p1) {
-						return sub[p1];
-					});
-					assets[newFile] = new RawSource(result);
-					callback();
-				}.bind(this));
-			}.bind(this), callback);
-		}.bind(this));
+	compiler.plugin("emit", function(compilation, callback) {
+		var assets = compilation.assets;
+		async.forEach(Object.keys(assets), function(file, callback) {
+			if(Array.isArray(this.test)) {
+				if(this.test.every(function(t) {
+					return !t.test(file);
+				})) return callback();
+			} else if(this.test && !this.test.test(file))
+				return callback();
+			var asset = assets[file];
+			var content = asset.source();
+			if(!Buffer.isBuffer(content))
+				content = new Buffer(content, "utf-8");
+			var originalSize = content.length;
+			if(originalSize < this.threshold) return callback();
+			this.algorithm(content, this.compressionOptions, function(err, result) {
+				if(err) return callback(err);
+				if(result.length / originalSize > this.minRatio) return callback();
+				var parse = url.parse(file);
+				var sub = {
+					file: file,
+					path: parse.pathname,
+					query: parse.query || ""
+				};
+				var newFile = this.asset.replace(/\[(file|path|query)\]/g, function(p0,p1) {
+					return sub[p1];
+				});
+				assets[newFile] = new RawSource(result);
+				callback();
+			}.bind(this));
+		}.bind(this), callback);
 	}.bind(this));
 };
